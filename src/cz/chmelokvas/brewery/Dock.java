69ac73d7fef556074943a507057940ac72acd258
Jan Dvorak <dvorakj50@gmail.com>
2014-10-29 11:01:25 +0100

    javadoc

--- src/cz/chmelokvas/brewery/Dock.java
+++ src/cz/chmelokvas/brewery/Dock.java
@@ -11,10 +11,24 @@ import java.util.TreeSet;
 import cz.chmelokvas.util.Route;
 
 public class Dock extends Stock {
-//	private Logger logger = Logger.getInstance();
+	
+	/**
+	 * Seznam instrukci, ktere autum predame zitra v osm
+	 */
 	private final List<List<Instruction>> tomorrow = new LinkedList<List<Instruction>>();
+	
+	/**
+	 * Pocet vytvorenych aut v dane hodine
+	 */
 	private int createdCars = 0;
 	
+	/**
+	 * Vytvori nove prekladiste na danych souradnicich a prida mu dane id pro kontroler
+	 * vlozi sebe samo na prvni index v poli svych zakazniku
+	 * @param idCont id u kontroleru
+	 * @param x x-ova souradnice
+	 * @param y y-ova souradnice
+	 */
 	public Dock(int idCont, float x, float y){
 		this.provider = this;
 		this.idCont = idCont;
@@ -26,14 +40,12 @@ public class Dock extends Stock {
 	
 	/**
 	 * Provede vsechny udalosti, co se staly za dany krok v case to jest:
-	 * <li>Urci cestu nakladniho automobilu</li>
-	 * <li>Vysle dalsi auta na cestu</li>
-	 * <li>Posune auta o dany casovy usek</li>
+	 * <li>Urci cestu nakladniho automobilu
+	 * <li>Vysle dalsi auta na cestu
+	 * <li>Posune auta o dany casovy usek
 	 */
 	public void checkTimeEvents(){
-//		System.out.println(orders);
-		//Pocitani a planovani cest
-//		Time t = new Time(0,16,0);
+		//Predani instrukci ze vcerejska
 		if(c.mainTime.getHour() == 8){
 			for(List<Instruction> inst:tomorrow){
 				Car c = getFirstWaitingTruck();
@@ -42,8 +54,8 @@ public class Dock extends Stock {
 			tomorrow.remove(tomorrow);
 		}
 		
+		//vyrizovani objednavek
 		if(c.mainTime.getHour() > 8 && c.mainTime.getHour() < 16){
-//			createInstructions(orders);
 			prepareOrders();
 			logger.log(c.mainTime, 6, this + " vytvorilo " + createdCars + " novych aut");
 			createdCars = 0;
@@ -55,30 +67,34 @@ public class Dock extends Stock {
 		moveCars();
 	}
 	
-	public void prepareOrders(){
+	/**
+	 * Pro vsechny jiz prijmute objednavky urci cesty, kterymi budou reseny
+	 */
+	private void prepareOrders(){
 		
 		while(!orders.isEmpty()){
+			//vybrane objednavky musi byt serazeny podle vzdalenosti od prekladiste
 			SortedSet<Order> selected = new TreeSet<Order>(cmp);
 			Order o = orders.last();
 			selected.add(o);
 			orders.remove(o);
 			Pub p = o.getPub();
 			
+			//pokud sou u nejvzdalenejsi hospody sousedi, kteri jiz objednali zahrn je take do cesty
 			checkNeighbours(p,selected);
 			
 			Stack<Integer> s = new Stack<Integer>();
 			
 			prepareStackForPath(0, p.idProv, s);
 			
+			//vezme hospody po ceste a pokud si take objednaly tak je take prida
 			while(!s.isEmpty()){
 				int i = s.pop();
 				Pub tmp = ((Pub)customers.get(i));
-				checkPub(tmp,selected);
-//				checkNeighbours(tmp,selected);
-				
-				
+				checkPub(tmp,selected);		
 			}
 			
+			//vybrane objednavky oznaci jako za pripravovane a zacne pripravovat instrukce pro cestu auta
 			beingPrepared.addAll(selected);
 			createInstructions(selected);
 			
@@ -86,19 +102,32 @@ public class Dock extends Stock {
 		}
 	}
 	
+	/**
+	 * Zjisti zda dana hospoda jiz objednala a jestli ano vlozi ji do dane mnoziny
+	 * @param p Zkoumana hospoda
+	 * @param selected Mnozina kam ji pridavame
+	 */
 	private void checkPub(Pub p, Set<Order> selected){
 		Order today = p.getTodayOrder();
 		Order yesterday = p.getYesterdayOrder();
+		//zkontroluje zda jiz neobjednala dnes
 		if(today != null && orders.contains(today)){
 			selected.add(today);
 			orders.remove(today);
 		}
+		//zkontroluje zda neobjednala vcera
 		if(yesterday != null && orders.contains(yesterday)){
 			selected.add(yesterday);
 			orders.remove(yesterday);
 		}
 	}
 	
+	/**
+	 * Pro vsechny sousedy hospody p ze stejneho sektoru zkontrolujeme jestli take neobjednali
+	 * a kdyz ano tak je prida k vybranym
+	 * @param p Hospoda jejiz sousedy zkoumame
+	 * @param selected Vybrane hospody pro cestu
+	 */
 	private void checkNeighbours(Pub p, Set<Order> selected){
 		for(Route r: p.routes){
 			TransportNode neighbourN = c.nodes.get(r.getValue());
@@ -112,7 +141,7 @@ public class Dock extends Stock {
 	/**
 	 * Pokud dane auto za dany krok casu neco udelalo, jsou vypsany vykonane instrukce a je zadana nova instrukce
 	 */
-	public void moveCars(){
+	private void moveCars(){
 		for(Car car : garage){
 			
 			Instruction i = car.getCurrentInstruction();
@@ -126,6 +155,11 @@ public class Dock extends Stock {
 		}
 	}
 	
+	/**
+	 * Predane auto dokonci danou instrukci
+	 * @param car Predane auto
+	 * @param i Instrukce, kterou ma auto vykonat
+	 */
 	private void finishInstruction(Car car, Instruction i){
 		car.setPosition(i.getDestination());
 		
@@ -140,11 +174,17 @@ public class Dock extends Stock {
 		
 		logger.log(i.getFinished(), 4, car + " " + i.getState().getStrFin() + " " + i.getDestination());
 		
+		//pokud byla u instrukce prilozena objednavka tak ji vyrid
 		if(i.getOrder() != null){
 			deliverOrder(i.getOrder(),car);
 		}
 	}
 	
+	/**
+	 * Prechod daneho auta na dalsi instrukci
+	 * @param car Auto, kteremu zaridime prechod na dalsi instrukci
+	 * @return Instrukci kterou ma auto provest dale
+	 */
 	private Instruction startNewInstructions(Car car){
 		Time time = car.getCurrentInstruction().getFinished();
 		car.getInstructions().remove(0);
@@ -170,7 +210,7 @@ public class Dock extends Stock {
 	 * @param orders Objednavky, ktere ma dane auto stihnout
 	 * @return Celkovy pocet, ktery mame do auta nalozit
 	 */
-	public int checkCarCapacity(Set<Order> orders){
+	private int checkCarCapacity(Set<Order> orders){
 		int sum = 0;
 		for(Iterator<Order> it = orders.iterator(); it.hasNext();){
 			Order o = it.next();
@@ -191,16 +231,16 @@ public class Dock extends Stock {
 	 * Priradi prvnimu volnemu autu posloupnost instrukci, ktere ma provezt cestou do hospod
 	 * @param orders Vsechny objednavky ktere musi cestou vyridit
 	 */
-	public void createInstructions(Set<Order> orders){
+	private void createInstructions(Set<Order> orders){
 		//Urceni kolik ma auto nalozit sudu a pridani odpovidajicich instrukci
 		int sum = checkCarCapacity(orders);
-		List<Instruction> instructions = new LinkedList<Instruction>();
-		addFirstInstructions(instructions,c.mainTime, sum);
+		 List<Instruction> instructions = addFirstInstructions(c.mainTime, sum);
 		
-		//Oriznuti objednavek ktere nestihame + a vraceni posledni objednavky
-		Order order = checkTimeOfDelivery(orders, instructions);
+		//Oriznuti objednavek ktere nestihame
+		checkTimeOfDelivery(orders, instructions);
 		
-		//odebrani poctu sudu od objednavek, ktere vyrizovat nebudeme
+		//odebrani poctu sudu od objednavek, ktere vyrizovat nebudeme 
+		//(vyrizovane byly z mnoziny odebrany)
 		for(Order o:orders){
 			sum -= o.getAmount();
 		}
@@ -212,13 +252,21 @@ public class Dock extends Stock {
 
 		correctTime(instructions,sum);
 		
-		createInstructionsPathHome(instructions, order.getPub(), sum);
+		createInstructionsPathHome(instructions, sum);
 		
 		
 		Car c = getFirstWaitingTruck();
 		c.setInstructions(instructions);
 	}
 	
+	/**
+	 * Projde mnozinu objednavek, a zkusi pro kazdy prvek vypocitat cestu.<br>
+	 * Pokud zjisti, ze se to neda stihnout, zkusi to naplanovat na zitra.<br>
+	 * Pokud bychom to ani zitra nestihli, doruci to jeste dnes ale opozdene
+	 * @param orders Mnozina vyrizovanych objednavek
+	 * @param instructions Instrukce, ktere budeme modifikovat pro cesty k vyrizeni danych objednavek
+	 * @return Posledni objednavka, ktera bude dnes vyrizena
+	 */
 	private Order checkTimeOfDelivery(Set<Order> orders, List<Instruction> instructions){
 		Order order = null;
 		for(Iterator<Order> it = orders.iterator(); it.hasNext();){
@@ -228,7 +276,11 @@ public class Dock extends Stock {
 			if(instructions.get(instructions.size()-1).getFinished().getHour() >= 16){
 				SortedSet<Order> tmpOrd = new TreeSet<Order>(cmp);
 				tmpOrd.addAll(orders);
+				
+				//Zkus odeslat zitra
 				List<Instruction> tomorrow = deliverTomorrow(tmpOrd);
+				
+				//Pokud bychom to nestihli, udelej to dnes ale dorazis pozdeji
 				if(tomorrow != null){
 					this.tomorrow.add(tomorrow);
 					removeUnnecessaryInstr(instructions);
@@ -237,30 +289,47 @@ public class Dock extends Stock {
 			}
 			
 			order = o;
+			//Pokud bude cesta realizovana, odstranime tuto objednavku z mnoziny
 			it.remove();
 		}
 		
 		return order;
 	}
 	
-	private void addFirstInstructions(List<Instruction> instructions, Time t, int sum){
+	
+
+	/**
+	 * Vytvori pocatecni instrukce zacinajici v case t.<br>
+	 * Prvni instrukce bude, ze bude cekat do daneho casu<br>
+	 * Dale bude nakladat sum*(rychlost nakladani) minut
+	 * @param t Cas kdy ma byt ukoncena prvni instrukce
+	 * @param sum Mnozstvi, ktere ma auto pomoci druhe instrukce nalozit
+	 * @return Spojovy seznam instrukci
+	 */
+	private List<Instruction> addFirstInstructions(Time t, int sum){
+		List<Instruction> instructions = new LinkedList<Instruction>();
 		int loadingMinutes = sum*CarType.TRUCK.getReloadingSpeed();
 		
+		//vytvareni uplne prvni instrukce
 		Time tmpTime = new Time(t.value());
 		instructions.add(0,new Instruction(State.WAITING, this, tmpTime));
 		
+		//vytvareni instrukce nakladani
 		tmpTime = tmpTime.getTimeAfterMinutes(loadingMinutes);
-		
 		instructions.add(1,new Instruction(State.LOADING, this, sum, tmpTime));
+		
+		return instructions;
 	}
 	
+	/**
+	 * Zkusi naplanovat doruceni danych objednavek pristi den
+	 * @param orders Mnozina vyrizovanych objednavek
+	 * @return Seznam  Instrukci pro cestu zitra, nebo null, pokud bychom to zitra nestihli
+	 */
 	private List<Instruction> deliverTomorrow(Set<Order> orders){
-	
 		int sum = checkCarCapacity(orders);
-		List<Instruction> instructions = new LinkedList<Instruction>();
-		addFirstInstructions(instructions, new Time(c.mainTime.getDay() + 1, 8, 0), sum);
+		List<Instruction> instructions = addFirstInstructions(new Time(c.mainTime.getDay() + 1, 8, 0), sum);
 				
-		Order order = null;
 		for(Iterator<Order> it = orders.iterator(); it.hasNext();){
 			Order o = it.next();
 			createTravellingInstructions(instructions,o);
@@ -268,19 +337,28 @@ public class Dock extends Stock {
 				//nestihame
 				return null;
 			}
-			order = o;
 			it.remove();
 		}
-		createInstructionsPathHome(instructions, order.getPub(), sum);
+		createInstructionsPathHome(instructions, sum);
 
 		return instructions;
 	}
 	
-	
+	/**
+	 * Vrati seznam vsech seznamu instrukci na zitrek
+	 * @return Seznam vsech seznamu instrukci na zitrek
+	 */
 	public List<List<Instruction>> getTomorrow() {
 		return tomorrow;
 	}
-
+	
+	
+	/**
+	 * Pokud jsme modifikovali mnozstvi objednavek, ktere chceme dorucit,
+	 * musime posunout instrukce v case, jelikoz se nam timpadem zmenila doba nakladani
+	 * @param instructions Seznam instrukci ktery chceme modifikovat
+	 * @param sum Nove mnozstvi sudu, ktere je treba nalozit
+	 */
 	private void correctTime(List<Instruction> instructions, int sum){
 		int correct = instructions.get(1).getFinished().value() - c.mainTime.getTimeAfterMinutes(sum*CarType.TRUCK.getReloadingSpeed()).value();
 
@@ -291,6 +369,11 @@ public class Dock extends Stock {
 		}
 	}
 	
+	/**
+	 * Z daneho seznamu odstrani nepotrebne instrukce,
+	 * ktere slouzily k obslouzeni posledni objednavky, ktera jiz vyrizovana nebude
+	 * @param instructions
+	 */
 	private void removeUnnecessaryInstr(List<Instruction> instructions){
 		LinkedList<Instruction> linkedInstructions = (LinkedList<Instruction>)instructions;
 		do{
@@ -299,11 +382,13 @@ public class Dock extends Stock {
 	}
 	
 	/**
-	 * Vezme danou polohu auta a vypocte pro nej instrukce tak aby se dostal do mista, ktere danou objednavku objednalo
-	 * @param c Auto kteremu budeme davat instrukce
-	 * @param o Objednavka, kterou musi auto vyridit
+	 * Z mista, kde by se auto nachazelo po vykonani posledni instrukce v {@code instructions}
+	 * zjistime cestu do mista, kde mame vyridit objednavku o a pro kazdy prujezd uzlem
+	 * musime vypocitat instrukci kdy jim projede
+	 * @param instructions Seznam instrukci pro auto
+	 * @param o Dalsi objednavka ktera ma byt vyrizena
 	 */
-	public void createTravellingInstructions(List<Instruction> instructions, Order o){
+	private void createTravellingInstructions(List<Instruction> instructions, Order o){
 
 		Instruction in = ((LinkedList<Instruction>)instructions).getLast();
 		
@@ -324,6 +409,10 @@ public class Dock extends Stock {
 		
 		Time t = instructions.get(instructions.size() -1).getFinished();
 		
+		/* Instrukce vyrizovani objednavky.
+		 * U posledni instrukce (nakladani prazdnych sudu) je dana i objednavka.
+		 * Auto tak vi, ze po vykonani teto instrukce je objednavka vyrizena
+		 */
 		t = t.getTimeAfterMinutes(reloadingMinutes);
 		instructions.add(new Instruction(State.UNLOADING,o.getPub(), o.getAmount(), t));
 		t = t.getTimeAfterMinutes(reloadingMinutes);
@@ -332,12 +421,13 @@ public class Dock extends Stock {
 	}
 	
 	/**
-	 * Naplni zasobnik id bodu, ktere musi auto navstivit, pokud chce dorazit z bodu {@code source} do bodu {@code destination}
+	 * Naplni zasobnik id bodu, ktere musi auto navstivit,
+	 * pokud chce dorazit z bodu {@code source} do bodu {@code destination}
 	 * @param source id pocatecniho bodu
 	 * @param destination id ciloveho bodu
 	 * @param nodes zasobnik kde budeme ukladat uzly
 	 */
-	public void prepareStackForPath(int source, int destination, Stack<Integer> nodes){
+	private void prepareStackForPath(int source, int destination, Stack<Integer> nodes){
 		int i = destination;
 		nodes.push(i);
 		//poradi je treba prohodit, proto se uziva zasobniku
@@ -348,17 +438,27 @@ public class Dock extends Stock {
 		}
 	}
 	
-	public void createInstructionsPathHome(List<Instruction> instructions, TransportNode n, int sum){
-		int source = n.idProv;
+	
+	/**
+	 * Vytvori instrukce pro cestu 
+	 * a posleze i instrukce pro vylozeni daneho mnozstvi prazdnych sudu
+	 * @param instructions Dany seznam instrukci ktery modifikujeme 
+	 * @param sum Mnozstvi prazdnych sudu, ktere je treba vylozit
+	 */
+	private void createInstructionsPathHome(List<Instruction> instructions, int sum){
+		Instruction last = ((LinkedList<Instruction>)instructions).getLast();
+		
+		int source = last.getDestination().idProv;
 		int destination = 0;
+		
+		Time t = last.getFinished();
+		
 		Stack<Integer> nodes = new Stack<Integer>();
 		
 		prepareStackForPath(source, destination, nodes);
 		
 		addPathInstructions(source, instructions, nodes);
-		
-		Time t = ((LinkedList<Instruction>)instructions).getLast().getFinished();
-		
+				
 		t = t.getTimeAfterMinutes(sum*CarType.TRUCK.getReloadingSpeed());
 		
 		instructions.add(new Instruction(State.UNLOADING_EMPTY_BARRELS, this, sum, t));
@@ -372,7 +472,7 @@ public class Dock extends Stock {
 	 * @param c Auto, kteremu dane instrukce predame
 	 * @param nodes Zasobnik, ktery obsahuje uzly, pres ktere auto pojede
 	 */
-	public void addPathInstructions(int i, List<Instruction> instructions, Stack<Integer> nodes){
+	private void addPathInstructions(int i, List<Instruction> instructions, Stack<Integer> nodes){
 		Time t = ((LinkedList<Instruction>)instructions).getLast().getFinished();
 		int x = i, y = 0;
 		while(!nodes.empty()){
@@ -392,7 +492,7 @@ public class Dock extends Stock {
 	 * Vrati prvni cekajici nakladak
 	 * @return prvni cekajici nakladak
 	 */
-	public Car getFirstWaitingTruck(){
+	private Car getFirstWaitingTruck(){
 		for(Car c: garage){
 			if(c.getCurrentInstruction() == null && c.getPosition().equals(this)){
 				return c;
